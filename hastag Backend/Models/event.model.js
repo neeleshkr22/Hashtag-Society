e211e405e847d412fd174630272b1ff472253564
Neelesh <130912265+neeleshkr22@users.noreply.github.com>
2025-03-09 15:38:11 +0530

    update

---
+++ hastag Backend/Models/event.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema({
+    eventName :{
+        type: String,
+        required: true
+    },
+    eventDate:{
+        type: Date,
+        required: true
+    },
+    eventTime:{
+        type: String,
+        required: true
+    },
+    eventLocation:{
+        type: String,
+        required: true
+    },
+    eventDescription:{
+        type: String,
+        required: true
+    },
+})
+
+export default mongoose.model('Event', eventSchema);
